90052b8693ba5b46cfcb4e758b8cbb0762f9a0d0
yuNNNu <nico.pobletem@alumnos.duoc.cl>
2020-11-14 19:12:50 -0300

    instalacion de express, conexión a mongoose y body-parser

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("hola mundo");
+/*=============================================
+=         INSTANCIAS DE REQUERIMIENTOS        =
+=============================================*/
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+/*=============================================
+=VARIABLE PARA LAS FUNCIONALIDADES DE EXPRESS =
+=============================================*/
+
+const app = express();
+
+/*=============================================
+=        MIDDLEWARES PARA BODY-PARSER         =
+=============================================*/
+
+app.use(bodyParser.urlencoded({ limit:'10mb', extend:true}));
+app.use(bodyParser.json({limit:'10mb', extend:true}));
+
+
+/*=============================================
+=   ESQUEMA PARA MODELO CONECTOR A MONGODB    =
+=============================================*/
+
+let Schema = mongoose.Schema;
+let slideSchema = new Schema({
+
+})
+
+/*=============================================
+=            CONEXIÓN A BASE DE DATOS         =
+=============================================*/
+mongoose.connect('mongodb://localhost:27017/fit_project', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, (err, res) => {
+
+	if(err) throw err;
+	console.log("Conectado a la base de datos");
+});
+
+/*=============================================
+=           SALIDA AL PUERTO HTTP             =
+=============================================*/
+
+app.listen(4000, ()=> {
+	console.log('Puerto 4000 habilitado');
+})
