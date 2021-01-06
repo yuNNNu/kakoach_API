abb19271bfac15749ab22d5c192b73bbd3b0ba14
Luciano Molina <lucianoma63@gmail.com>
2021-01-06 19:02:03 -0300

    put de planes segundarios validan que sea un id de planes existentes

--- controlador/inicio/planes_secundarios.controlador.js
+++ controlador/inicio/planes_secundarios.controlador.js
@@ -1,7 +1,7 @@
 /*=============================================
 IMPORTAMOS EL MODELO
 =============================================*/
-
+const planes = require('../../modelo/planes/planes.modelo');
 const plan = require('../../modelo/inicio/planes_secundarios.modelo')
 /*=============================================
 FUNCIÓN GET
@@ -55,69 +55,104 @@ let updateSecondaryPlan = (req, res) =>
     let _id = req.params.id;
     // obtenemos el cuerpo del formulario
     let body = req.body;
-        //01  VALIDAMOS EXISTENCIA DE BENEFICIO
-        plan.findById(_id, (err, data) =>
-        {
-         
+    let idPlan = body.id;
+    //01  VALIDAMOS EXISTENCIA DE BENEFICIO
+    plan.findById(_id, (err, data) =>
+    {
         
-             //Validammos que no haya error
-            if (err) {
-                return res.json({
-                    status: 500,
-                    mensaje: "Error en el servidor",
-                    err
-                })
-            }
-            //Validamos que la beneficio exista
-            if (!data) {
-                return res.json({
-                    status: 404,
-                    mensaje: "No existe el plan en la base de datos",
-                    err
-                })
-            }
-            
+    
+            //Validammos que no haya error
+        if (err) {
+            return res.json({
+                status: 500,
+                mensaje: "Error en el servidor",
+                err
+            })
+        }
+        //Validamos que la beneficio exista
+        if (!data) {
+            return res.json({
+                status: 404,
+                mensaje: "No existe el plan en la base de datos",
+                err
+            })
+        }
+       
+        // 02 VALIDAR CAMBIO
+        let validarCambio = (_id, idPlan) => {
+          return new Promise((resolve, reject) => {
+            let datosplan = {
+              id: idPlan,
+            };
+              
+              planes.findById(idPlan, (err, data) =>
+              {
                 
-            // 03 ACTUALIZAR REGISTROS
-            let cambiarRegistroBD = (_id, body) =>
-            {
-                return new Promise((resolve, reject) =>
+                if (err) {
+                    let respuesta = {
+                    res: res,
+                    mensaje: "Error en el servidor o el id es invalido",
+                    };
+                    reject(respuesta);
+                }
+                if (!data)
                 {
-                    let datosplan = {
-                        id : body.id
-                    }
-                    //Actualizamos en MongoDB
-                    //https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
-                    plan.findByIdAndUpdate(_id, datosplan, {
-                        new: true, // Con esto me muestra lo que se guardo y no el antiguo
-                        runValidators: true // Con esto me muestra lo que se guardo y no el antiguo           
-                    }, (err, data) =>
-                    {
-                        if (err) {
-                            let respuesta = {
-                            res: res,
-                            error: err
-                            }
-                            reject(respuesta);
+                    
+                    let respuesta = {
+                    res: res,
+                    mensaje: "No existe el plan ingresado",
+                    };
+                    reject(respuesta);
+                    
+                }
+                resolve(idPlan)
+            })
+           
+          });
+        };   
+        // 03 ACTUALIZAR REGISTROS
 
-                        }
-
+        let cambiarRegistroBD = (_id, idPlan) =>
+        {
+            return new Promise((resolve, reject) =>
+            {
+                let datosplan = {
+                    id : idPlan
+                }
+                //Actualizamos en MongoDB
+                //https://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
+                plan.findByIdAndUpdate(_id, datosplan, {
+                    new: true, // Con esto me muestra lo que se guardo y no el antiguo
+                    runValidators: true // Con esto me muestra lo que se guardo y no el antiguo           
+                }, (err, data) =>
+                {
+                    if (err) {
                         let respuesta = {
                         res: res,
-                        data: data
-                        }    
-                        resolve(respuesta)
-                    })
-                })        
-            }
+                        error: err
+                        }
+                        reject(respuesta);
 
-            cambiarRegistroBD(_id, body).then(respuesta =>
+                    }
+
+                    let respuesta = {
+                    res: res,
+                    data: data
+                    }    
+                    resolve(respuesta)
+                })
+            })        
+        }
+        // SINCRONIZACION
+        validarCambio(_id, idPlan).then(idPlan =>
+        {
+            cambiarRegistroBD(_id, idPlan).then(respuesta =>
             {
-                console.log("res",respuesta)
+                
                 respuesta["res"].json({
                 status: 200,
                 data: respuesta["data"],
-                mensaje: "El plan personal ha sido actualizado con exito"
+                mensaje: "El plan fue actualizado cone exito!"
                 })
             }).catch((respuesta =>
             {
@@ -128,8 +163,21 @@ let updateSecondaryPlan = (req, res) =>
                 })
             }))
 
-               
-    }   )
+        }).catch(respuesta =>
+        {
+                respuesta["res"].json({
+                status:400,
+                mensaje:respuesta["mensaje"]
+
+            })
+        })
+
+
+
+
+
+            
+    })
 }
 /*=============================================
 FUNCIÓN POST
