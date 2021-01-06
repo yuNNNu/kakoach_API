e591fe6e1ac96d5b10e76cc639f9e210a1e5d112
Luciano Molina <lucianoma63@gmail.com>
2021-01-06 20:24:00 -0300

    arreglandoi paso de datos debido a que el input envia solo mm-yyyy

--- controlador/estadisticas/estadisticas.controlador.js
+++ controlador/estadisticas/estadisticas.controlador.js
@@ -18,7 +18,7 @@ let All = (req, res) =>
   }
   const fechaInicio = body.fecha_inicio;
   const fechaFin = body.fecha_fin;
-  console.log(fechaInicio)
+ 
   Venta.find({}).exec((err, data) =>
   {
     
@@ -109,11 +109,125 @@ let All = (req, res) =>
   
 
 }
+let AllByMonth = (req, res) =>
+{
+ 
+  const body = req.body;
+ 
+  if (!body.fecha_filtro )
+  {
+      return res.json({
 
+			status:400,
+			mensaje: "Requiere de fecha de inicio y fecha fin" 
+			})
+  }
+ 
+  const fechaFiltro = new Date(body.fecha_filtro);
+
+  const annoFiltro = fechaFiltro.getFullYear()+1;
+  const mesFiltro = fechaFiltro.getMonth() - 10;
+  
+
+
+
+
+  Venta.find({}).exec((err, data) =>
+  {
+    
+    // Si ocurre un error
+    if (err) {
+      return res.json({
+        status: 500,
+        mensaje: "Error en la petición",
+      });
+    }
+  
+
+    // inicializacion de variable  total
+    let total = 0;
+    let planesEnRango = [];
+    let PlanesFilter = [];
+    
+    // Llenamos dos arreglos 1 de todos los planes y hasta repetidos y 1 arreglo de planes no repetidos
+    data.map((x) =>
+    {
+  
+      const fecha = x.fecha_venta;
+   
+      // fechas que deben ser recepcionadas con req.body
+      var anno = fecha.getFullYear();
+      var mes = fecha.getMonth() + 1;
+      
+      if (anno ==annoFiltro && mes == mesFiltro) {
+        total = x.precio + total;
+
+        planesEnRango.push({ id: x.id_plan,
+                            nombre: x.nombre_plan});
+
+        if (!PlanesFilter.includes(x["id_plan"])) {
+          PlanesFilter.push(x.id_plan);
+        }
+      
+      }
+    });
+
+    // Contamos la cantidad por plan vendido, retorna arr con obj de idPlan y cantidad
+    let planesYCantidad = PlanesFilter.map((idPlan) => {
+      var cont = 0;
+      var nombre = "";
+      
+      planesEnRango.forEach((plan) => {
+        if (plan["id"] == idPlan) {
+          cont++;
+          nombre= plan["nombre"]
+        }
+      });
+        
+  
+        let obj = {
+          nombre: nombre,
+          cantidad: cont,
+          id: idPlan
+        };
+
+      return obj;
+    });
+
+    // ordenamos planes de mayor cant a menor
+    planesYCantidad.sort((a, b) => {
+      return b.cantidad - a.cantidad;
+    });
+
+    const largo = planesYCantidad.length;
+    const cantVentas = planesEnRango.length;
+    if (cantVentas <= 0)
+    {
+        return res.json({
+
+        status:404,
+        mensaje: "No hay registros en esas fechas" 
+        
+        })
+    }
+    res.json({
+      status: 200,
+      cantidad_ventas: cantVentas,
+      total_ventas: total,
+      mas_vendido: planesYCantidad[0],
+      menos_vendido: planesYCantidad[largo - 1],
+      planes_y_cantidad: planesYCantidad,
+    });
+  });
+
+  
+
+}
 
 /*========================
 EXPORTAMOS FUNCIONES DEL CONTROLADOR
 ========================== */
 module.exports = {
-    All
+  All,
+  AllByMonth
 }
--- ruta/estadisticas/estadisticas.rutas.js
+++ ruta/estadisticas/estadisticas.rutas.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 const Estadistica = require('../../controlador/estadisticas/estadisticas.controlador');
 app.post('/estadisticas', Estadistica.All);
+app.post('/estadisticas-mensual', Estadistica.AllByMonth);
 
 module.exports = app;
